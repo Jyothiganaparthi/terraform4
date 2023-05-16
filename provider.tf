c25a6528e3abdb3cd70be97292c0a8bf16743339
Jyothiganaparthi <124667728+Jyothiganaparthi@users.noreply.github.com>
2023-05-16 22:15:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   version    = "~> 4.0"
   region     = "us-east-2"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = "AKIA2NHV5FNI2QT2KK52"
+  secret_key = "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"
 
 }
 terraform {
