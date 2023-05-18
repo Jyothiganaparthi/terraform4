4e7842e39cb363e37e5849b4051df6264fdb8085
Jyothiganaparthi <124667728+Jyothiganaparthi@users.noreply.github.com>
2023-05-18 15:11:50 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "example" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
+  cidr_block           = var.cidr
+  enable_dns_hostnames = var.enable
   tags = {
     Name = "Myvpc"
   }
